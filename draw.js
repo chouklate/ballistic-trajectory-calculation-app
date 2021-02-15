6c19fa8de0d663f248056021f84571a1d8dd938e
chouklate <77095432+chouklate@users.noreply.github.com>
2021-02-16 08:17:10 +0900

    Add files via upload

---
+++ draw.js
@@ -0,0 +1,132 @@
+var r = globalParams.simulationSpeed;
+
+var i = 0;
+
+var mouseDownPos = {x:0, y:2000};
+
+
+function initCanvas()
+{
+    
+
+    bulletImg = new Image();
+    bulletImg.src = "bullet.png";
+
+    bulletImg.onload = e =>
+    {
+        targetImg = new Image();
+        targetImg.src = "target.png";
+
+        targetImg.onload = e =>
+        {
+            launcherImg = new Image();
+            launcherImg.src = "launcher.png"
+            launcherImg.onload = e =>
+            {
+                canvas = document.getElementById("main-canvas");
+                bgcanvas = document.getElementById("bg-canvas");
+                bgctx = bgcanvas.getContext("2d");
+                ctx = canvas.getContext("2d");
+                canvas.addEventListener("mousedown", e => {mouseDownPos = {x: e.offsetX, y: canvas.height - e.offsetY};resetCanvas()});
+                
+                document.addEventListener("keydown", e => {
+                    e.preventDefault();
+                    if(e.key == "a") {targetList[0].velocity.x -= 5}
+                    if(e.key == "d") {targetList[0].velocity.x += 5}
+                    if(e.key == "w") {targetList[0].velocity.y += 9}
+                    if(e.key == "s") {targetList[0].velocity.y -= 5}
+                    console.log(targetList[0].velocity.x)
+
+                });
+
+
+                bgctx.beginPath(); // draw launchers
+                launcherList.forEach(e => {
+                    bgctx.drawImage(launcherImg, e.pos.x - .5*launcherImg.width, canvas.height-e.pos.y - .5*launcherImg.height)
+                });
+
+                timerID = setInterval("updateCanvas()", 1000/globalParams.FPS);
+
+                managerID = setInterval("bulletManager()", 5000/globalParams.FPS);
+
+                scheduleID = setInterval("b()", 1000/globalParams.fireRate);
+            }
+        }
+    }
+}
+
+function b() //debug
+{
+    launcherList[3].Fire(omnidirectional = false)
+    //i++;
+    
+}
+function updateCanvas()
+{
+    applyPhysics();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+    ctx.beginPath(); // draw bullets
+
+    bulletList.forEach(e => {
+        e.pos.x += e.velocity.x * r;
+        e.pos.y += e.velocity.y * r;
+        ctx.moveTo(e.pos.x, canvas.height - e.pos.y);
+
+        
+        ctx.drawImage(bulletImg, e.pos.x - .5*bulletImg.width, canvas.height-e.pos.y - .5*bulletImg.height);
+    });
+
+    ctx.beginPath(); // draw target
+    targetList.forEach(e => {
+
+        e.pos.x += e.velocity.x * r;
+        e.pos.y += e.velocity.y * r;
+
+        
+        ctx.drawImage(targetImg, e.pos.x -.5*targetImg.width, canvas.height-e.pos.y -.5*targetImg.height);
+    })
+    
+}
+
+function applyPhysics()
+{
+bulletList.forEach(e => {
+    e.velocity.y += globalParams.gravity * r;
+})
+targetList.forEach(e => {
+    if(e.pos.y <= 0)
+    {
+        e.pos.y = 0;
+        e.velocity.x = 0;
+        e.velocity.y = 0;
+    }
+    else
+    {
+    e.velocity.y += globalParams.gravity * r;
+    }
+})
+}
+function bulletManager()
+{
+    for(let i=0; i<bulletList.length; i++)
+    {
+        e = bulletList[i]
+        if(e.pos.x < 0 || e.pos.x > canvas.length || e.pos.y > canvas.height || e.pos.y < 0)
+        {
+            bulletList.splice(i, 1);
+        }
+        
+    };
+    
+
+
+}
+
+function resetCanvas()
+{
+    //debug only for now
+    targetList[0].pos = mouseDownPos;
+    targetList[0].velocity.y = 0;
+}
---
+++ simulation.js
@@ -0,0 +1,73 @@
+class GameObject 
+{
+    constructor(pos = {x: 0, y: 0})
+    {
+        this.pos = pos;
+    }
+}
+
+class Launcher extends GameObject
+{
+    constructor(pos, launchVelocity, target)
+    {
+        super(pos);
+        this.launchVelocity = launchVelocity;
+        this.target = target;
+    }
+
+    TransformPoint(targetPos) // converts coords into local space
+    {
+        return {x: targetPos.x - this.pos.x, y: targetPos.y - this.pos.y}
+    }
+
+    Fire(omnidirectional = false)
+    {
+        let Tlocal = this.TransformPoint(this.target.pos);
+        if(!omnidirectional && Tlocal.y <= 0) { return; }
+        let r = Tlocal.y/Tlocal.x;
+        let a = (-r*this.target.velocity.x + this.target.velocity.y) / this.launchVelocity; //a = s-rc
+        let cos = Tlocal.x > 0 ? (-a*r + Math.sqrt(r**2-a**2+1))/(r**2+1) : (-a*r - Math.sqrt(r**2-a**2+1))/(r**2+1)
+        let sin = Tlocal.y > 0 ? (Math.sqrt(1-cos**2)) : -(Math.sqrt(1-cos**2))
+        bulletList.push(new Bullet(Object.create(this.pos), {x: this.launchVelocity * cos, y: this.launchVelocity * sin}));
+    }
+
+}
+
+class Target extends GameObject{
+    constructor(pos, velocity = {x: 0, y: 0})
+    {
+        super(pos);
+        this.velocity = velocity;
+    }
+
+
+};
+
+class Bullet extends GameObject{
+    constructor(pos, velocity = {x: 0, y: 0})
+    {
+        super(pos);
+        this.velocity = velocity;
+    }
+}
+
+var globalParams =
+{
+    gravity: -0.9,
+    FPS : 30,
+    simulationSpeed: .5, // physics updates every simSpeed * FPS times per second
+
+    fireRate: 10 //debug only, per second
+}
+
+var launcherList = new Array();
+var bulletList = new Array();
+var targetList = new Array();
+
+t = new Target({x:0, y:2000}, {x:30, y:10});
+l1 = new Launcher({x:0, y:0}, 100, t);
+l2 = new Launcher({x:500, y:0}, 100, t);
+l3 = new Launcher({x:1000, y:0}, 100, t);
+l4 = new Launcher({x:2000, y:0}, 100, t);
+launcherList.push(l1, l2, l3, l4);
+targetList.push(t)
